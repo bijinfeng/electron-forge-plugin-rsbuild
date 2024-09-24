9533233b38053d1c1398e235f302796ec40c0ba6
jfbi <jfbi@iflytek.com>
2024-09-24 11:41:01 +0800

    fix: delete renderer default entry config

--- example/rsbuild.renderer.config.ts
+++ example/rsbuild.renderer.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig(() => {
   return {
+    html: {
+      template: './index.html',
+    },
+    source: {
+      entry: {
+        index: 'src/renderer.ts',
+      },
+    },
     plugins: [pluginReact()],
   }
-});
+})
--- packages/rsbuild-plugin/src/config/rsbuild.renderer.config.ts
+++ packages/rsbuild-plugin/src/config/rsbuild.renderer.config.ts
@@ -1,28 +1,20 @@
-import { RsbuildConfig } from '@rsbuild/core';
+import type { RsbuildConfig } from '@rsbuild/core'
 
-import { pluginExposeRenderer } from "./rsbuild.base.config";
-import type { ConfigEnv } from "../config-types";
+import type { ConfigEnv } from '../config-types'
+import { pluginExposeRenderer } from './rsbuild.base.config'
 
-export function getConfig(forgeEnv: ConfigEnv<"renderer">): RsbuildConfig {
-  const { root, mode, forgeConfigSelf } = forgeEnv;
-  const name = forgeConfigSelf.name ?? '';
+export function getConfig(forgeEnv: ConfigEnv<'renderer'>): RsbuildConfig {
+  const { root, mode, forgeConfigSelf } = forgeEnv
+  const name = forgeConfigSelf.name ?? ''
 
   return {
     root,
     mode,
-    html: {
-      template: "./index.html"
-    },
-    source: {
-      entry: {
-        index: "src/renderer.ts"
-      }
-    },
     output: {
-      assetPrefix: "./",
+      assetPrefix: './',
       distPath: {
         root: `.rsbuild/renderer/${name}`,
-      }
+      },
     },
     plugins: [pluginExposeRenderer(name)],
   }
